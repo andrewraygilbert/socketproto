0c72be4b805b91b4cf47916b852fa79ce4f4cb96
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-04-13 10:29:50 -0500

    data service

--- apps/socketproto/src/app/data-service.service.ts
+++ apps/socketproto/src/app/data-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { InjectionToken } from '@angular/core';
+
+const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
+  providedIn: 'root',
+  factory: () => localStorage
+});
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+
 }
