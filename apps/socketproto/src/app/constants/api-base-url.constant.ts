f798319e707a23d5acfef0f2d3d8550d2df1f0bf
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-04-14 17:09:27 -0500

    change links to https

--- apps/socketproto/src/app/constants/api-base-url.constant.ts
+++ apps/socketproto/src/app/constants/api-base-url.constant.ts
@@ -1,5 +1,5 @@
 import { environment } from './../../environments/environment';
 
-export const API_BASE_URL = environment.production ? 'http://socketproto.herokuapp.com/api/' : 'http://localhost:3333/api/';
+export const API_BASE_URL = environment.production ? 'https://socketproto.herokuapp.com/api/' : 'http://localhost:3333/api/';
 
-export const BASE_URL = environment.production ? 'http://socketproto.herokuapp.com' : 'http://localhost:3333';
+export const BASE_URL = environment.production ? 'https://socketproto.herokuapp.com' : 'http://localhost:3333';
