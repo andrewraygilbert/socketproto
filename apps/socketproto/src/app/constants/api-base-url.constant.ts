1a0886becf2ec11946c0cb72c1350afb0bb943cd
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-04-14 16:52:08 -0500

    update urls

--- apps/socketproto/src/app/constants/api-base-url.constant.ts
+++ apps/socketproto/src/app/constants/api-base-url.constant.ts
@@ -1,3 +1,5 @@
-export const API_BASE_URL = process.env.NODE_ENV === 'production' ? process.env.API_BASE_URL : 'http://localhost:3333/api/';
+import { environment } from './../../environments/environment';
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:3333';
+export const API_BASE_URL = environment.production ? 'http://socketproto.herokuapp.com/api/' : 'http://localhost:3333/api/';
+
+export const BASE_URL = environment.production ? 'http://socketproto.herokuapp.com' : 'http://localhost:3333';
