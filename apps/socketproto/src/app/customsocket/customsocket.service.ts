b3838afb3b6da80371319d72a33df9adba00aba9
Andrew Gilbert <andrewgilbert@uchicago.edu>
2020-04-17 10:25:53 -0500

    improved event method generation

--- apps/socketproto/src/app/customsocket/customsocket.service.ts
+++ apps/socketproto/src/app/customsocket/customsocket.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Inject } from '@angular/core';
 import * as io from 'socket.io-client';
-import { DataService } from './../data-service.service';
-import { Observable, from, fromEvent } from 'rxjs';
+import { Observable, fromEvent } from 'rxjs';
 import { InjectionToken } from '@angular/core';
 import { BASE_URL } from './../constants/api-base-url.constant';
 
@@ -24,43 +23,54 @@ export class CustomsocketService {
   public onConnect: ObservableFx;
   public onDisconnect: ObservableFx;
   public roomJoined: ObservableFx;
-
   private baseUrl = BASE_URL;
+
+
+  private socketEvents = [{
+    methodName: 'getChats',
+    eventName: 'chatmsg'
+  },{
+    methodName: 'getErrors',
+    eventName: 'err'
+  },{
+    methodName: 'onConnect',
+    eventName: 'connect'
+  },{
+    methodName: 'onDisconnect',
+    eventName: 'disconnect'
+  },{
+    methodName: 'participantJoined',
+    eventName: 'participant_joined'
+  },{
+    methodName: 'roomJoined',
+    eventName: 'room_joined'
+  }];
 
   constructor(
     @Inject(BROWSER_STORAGE) private storage: Storage,
-  ) {}
+  ) { }
 
   public getToken(): string {
     return this.storage.getItem('proto_access_token');
   }
 
+  // create a socket connection to the server with auth token
   public initializeSocket() {
-    this.socket = io(this.baseUrl, { query: {
-      token: this.getToken()
-    }});
-    this.initializeSocketMethods();
+    this.socket = io(this.baseUrl, {
+      query: {
+        token: this.getToken()
+      }
+    });
+    this.createEventMethods(this.socketEvents);
   }
 
-  private initializeSocketMethods() {
-    this.getChats = (): Observable<any> => {
-      return fromEvent(this.socket.on(), 'chatmsg');
-    };
-    this.getErrors = (): Observable<any> => {
-      return fromEvent(this.socket.on(), 'err');
-    };
-    this.participantJoined = (): Observable<any> => {
-      return fromEvent(this.socket.on(), 'participant_joined');
-    };
-    this.onConnect = (): Observable<any> => {
-      return fromEvent(this.socket.on(), 'connect');
-    };
-    this.onDisconnect = (): Observable<any> => {
-      return fromEvent(this.socket.on(), 'disconnect');
-    };
-    this.roomJoined = (): Observable<any> => {
-      return fromEvent(this.socket.on(), 'room_joined');
-    };
+  // generate methods that return observables for socket events
+  private createEventMethods(events) {
+    for (const event of events) {
+      this[event.methodName] = (): Observable<any> => {
+        return fromEvent(this.socket.on(), event.eventName);
+      };
+    }
   }
 
   public closeSocket() {
@@ -69,8 +79,7 @@ export class CustomsocketService {
   }
 
   public openSocket() {
-    console.log('the token now', this.getToken());
-    console.log('opening the socket', this.socket);
+    console.log('opening the socket');
     this.socket.open(`${this.baseUrl}?token=${this.getToken()}`);
   }
 
